4df90e534363aa779c04603ba97dcc5fa5880c49
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-09-21 23:20:52 +0600

    Add files via upload
    added week8 day7 codes

---
+++ week-08/day7/A_Divide_and_Equalize.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define TC(t)          int t; cin >> t; for(int i = 1; i <= t; i++)
+#define pii            pair<int,int>
+#define ft             ios::sync_with_stdio(false);  cin.tie(NULL);
+#define endl             "\n"
+#define pb             push_back
+#define py             cout<<"YES\n";
+#define pn             cout<<"NO\n";
+#define vrev(v)        reverse(v.begin(),v.end());
+#define vsort(v)       sort(v.begin(),v.end());
+#define rsort(v)       sort(v.rbegin(),v.rend());
+#define vlowerB(v,x)   lower_bound(v.begin(),v.end(),x)
+#define vupperB(v,x)   upper_bound(v.begin(),v.end(),x)
+#define F              first
+#define S              second
+#define rep(i,a,b)     for(int i=a;i<=b;i++)
+#define rep2(i,b,a)    for(int i=b;i>=a;i--)
+#define vi             vector<int>
+#define zrbits(x)      __builtin_ctzll(x)
+const int MOD=1e9+7;
+void solve(){
+    int n; cin>>n;
+    map<int,int>cnt;
+    vi a(n);
+    rep(i,0,n-1) cin>>a[i];
+    rep(j,0,n-1){
+    for(int i=2;i*i<=a[j];i++){
+        if(a[j]%i==0){
+            while(a[j]%i==0){
+                cnt[i]++;
+                a[j]/=i;
+            }
+        }
+    }
+    if(a[j]>1){
+        cnt[a[j]]++;
+    }
+    }
+    bool ok=true;
+    for(auto [x,y]:cnt){
+        // cout<<x<<" "<<y<<endl;
+        if(y%n!=0){
+            ok=false;
+            break;
+        }
+    }
+    if(ok) py
+    else pn
+}
+int32_t main()
+{
+   ft
+   TC(t)
+   solve(); 
+    return 0;
+}
