4423a0b6c5fa1962ad59dea50ffb234a7d9e2225
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-10-23 23:38:40 +0600

    Add files via upload
    added week13 day3 codes

---
+++ week-13/day3/Unmedian.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define TC(t)          int t; cin >> t; for(int i = 1; i <= t; i++)
+#define pii            pair<int,int>
+#define ft             ios::sync_with_stdio(false);  cin.tie(NULL);
+#define endl             "\n"
+#define pb             push_back
+#define py             cout<<"YES\n";
+#define pn             cout<<"NO\n";
+#define vrev(v)        reverse(v.begin(),v.end());
+#define vsort(v)       sort(v.begin(),v.end());
+#define rsort(v)       sort(v.rbegin(),v.rend());
+#define vlowerB(v,x)   lower_bound(v.begin(),v.end(),x)
+#define vupperB(v,x)   upper_bound(v.begin(),v.end(),x)
+#define F              first
+#define S              second
+#define rep(i,a,b)     for(int i=a;i<=b;i++)
+#define rep2(i,b,a)    for(int i=b;i>=a;i--)
+#define vi             vector<int>
+#define zrbits(x)      __builtin_ctzll(x)
+const int MOD=1e9+7;
+void solve(){
+    int n; cin>>n;
+    vi a(n);
+    rep(i,0,n-1) cin>>a[i];
+    vi b(n);
+    rep(i,0,n-1){
+        b[i]=a[i];
+    }
+    vsort(b)
+    int l=0;
+    map<int,int>mp;
+    rep(i,0,n-1){
+        if(a[i]>b[l]){
+            mp[a[i]]++;
+        }
+        else l++;
+    }
+    for(auto [x,y]:mp){
+        cout<<x<<" "<<y<<endl;
+    }
+}
+int32_t main()
+{
+   ft
+   TC(t)
+   solve(); 
+    return 0;
+}
