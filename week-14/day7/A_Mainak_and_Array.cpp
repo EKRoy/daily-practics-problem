3de654369e315fe04c6648cf732e4816307021de
Eshwor Kumar Roy <ekroy16@gmail.com>
2024-11-02 23:31:20 +0600

    Add files via upload
    added week14 day7 codes

---
+++ week-14/day7/A_Mainak_and_Array.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define TC(t)          int t; cin >> t; for(int i = 1; i <= t; i++)
+#define pii            pair<int,int>
+#define ft             ios::sync_with_stdio(false);  cin.tie(NULL);
+#define endl             "\n"
+#define pb             push_back
+#define py             cout<<"YES\n";
+#define pn             cout<<"NO\n";
+#define vrev(v)        reverse(v.begin(),v.end());
+#define vsort(v)       sort(v.begin(),v.end());
+#define rsort(v)       sort(v.rbegin(),v.rend());
+#define vlowerB(v,x)   lower_bound(v.begin(),v.end(),x)
+#define vupperB(v,x)   upper_bound(v.begin(),v.end(),x)
+#define F              first
+#define S              second
+#define rep(i,a,b)     for(int i=a;i<=b;i++)
+#define rep2(i,b,a)    for(int i=b;i>=a;i--)
+#define vi             vector<int>
+#define zrbits(x)      __builtin_ctzll(x)
+const int MOD=1e9+7;
+void solve(){
+    int n; cin>>n;
+    vi a(n);
+    rep(i,0,n-1) cin>>a[i];
+    int ans=INT_MIN;
+    rep(i,0,n-1){
+        ans=max(ans,a[(i-1+n)%n]-a[i]);
+    }
+    rep(i,1,n-1){
+        ans=max(ans,a[i]-a[0]);
+    }
+    rep(i,0,n-2){
+        ans=max(ans,a[n-1]-a[i]);
+    }
+    cout<<ans<<endl;
+}
+int32_t main()
+{
+   ft
+   TC(t)
+   solve();
+    return 0;
+}
